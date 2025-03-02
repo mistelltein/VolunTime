f9f77bd3c4e0a60b2fc787a48b2be9aa51dbaaf2
Beka <bekamistelltein@gmail.com>
2025-03-02 22:22:43 +0600

    Initial commit

---
+++ src/main/java/com/example/voluntime/VolunTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.voluntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voluntime/VolunTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voluntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
